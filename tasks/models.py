e6ecb7f31768091c874c64d04f4c859884159909
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-25 11:09:42 +0300

    add new models task & tag

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=250)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    def __str__(self):
+        return f"{self.content} - {'Done' if self.is_done else 'Not Done'}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
