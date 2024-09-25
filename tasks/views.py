3fe0813f41dcb2bee2857028e2f0890633153d1a
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-25 11:41:40 +0300

    add def index to views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task, Tag
+
+
+def index(request):
+    context = {
+        "tasks": Task.objects.all().order_by("is_done", "-created_at" ),
+        "tags": Tag.objects.all(),
+    }
+    return render(request, "tasks/index.html", context)
