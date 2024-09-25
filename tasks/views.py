84416dd271b9bdf292972ee5e969116555866629
Vlada Bilai <vlada.belayy@gmail.com>
2024-09-25 15:27:24 +0300

    correct views

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,95 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+
+from tasks.forms import TaskForm, TagForm
 from tasks.models import Task, Tag
 
 
 def index(request):
     context = {
         "tasks": Task.objects.all().order_by("is_done", "-created_at" ),
-        "tags": Tag.objects.all(),
     }
     return render(request, "tasks/index.html", context)
+
+
+def task_create(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:index")
+    else:
+        form = TaskForm()
+    return render(request, "tasks/task_form.html", {"form": form})
+
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:index")
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "tasks/task_form.html", {"form": form})
+
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect("tasks:index")
+    return render(request, "tasks/task_confirm_delete.html", {"task": task})
+
+
+def task_complete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = True
+    task.save(update_fields=["is_done"])
+    return redirect("tasks:index")
+
+
+def task_undo(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = False
+    task.save(update_fields=["is_done"])
+    return redirect("tasks:index")
+
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, "tasks/tag_list.html", {"tags": tags})
+
+
+def tag_create(request):
+    if request.method == "POST":
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:index")
+    else:
+        form = TagForm()
+    return render(request, "tasks/tag_form.html", {"form": form})
+
+
+def tag_update(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    if request.method == "POST":
+        form = TagForm(request.POST, instance=tag)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:tag_list")
+    else:
+        form = TagForm(instance=tag)
+    return render(request, "tasks/tag_form.html", {"form": form})
+
+
+def tag_delete(request, pk):
+    tag = get_object_or_404(Tag, pk=pk)
+    if request.method == "POST":
+        tag.delete()
+        return redirect("tasks:tag_list")
+    return render(request, 'tasks/tag_confirm_delete.html', {"tag": tag})
